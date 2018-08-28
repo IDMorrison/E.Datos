35a7fae9550da2ffca307910ef038f582af33480
IDMorrison <ariel.sadran@gmail.com>
2018-08-27 21:51:33 -0300

    2do commit

---
+++ ejerciciotabla.java
@@ -0,0 +1,14 @@
+import java.util.*;
+  
+  public class ejerciciotabla {
+  public static void main (String[] args){
+    Scanner teclado = new Scanner (System.in);
+    int num=0;
+    int i=0;
+    System.out.println("Ingrese un numero");
+    num=teclado.nextInt();
+    for (i=0; i<=10; i++) {
+      System.out.println(num+" x "+i+" = "+(num*i));
+    }
+  }
+  }  
