bd76608a1f0dcb9bab1306fbc85aeb4613b81606
26132765 <vilad765@gmail.com>
2018-09-03 21:40:12 -0300

    Clase 03/09

---
+++ clase 03.09/clase03_09_2.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class clase03_09_2 {
+  public static void main (String[] args){
+    Scanner teclado = new Scanner (System.in);
+    
+    System.out.println("Ingrese un numero entre 0 y 99");
+    String num1 = teclado.nextLine();
+    
+    
+  
+  }
+
+
+}
---
+++ clase 31.08/Nombre.java~
@@ -0,0 +1,3 @@
+public class Nombre {
+  String nombre;
+}
